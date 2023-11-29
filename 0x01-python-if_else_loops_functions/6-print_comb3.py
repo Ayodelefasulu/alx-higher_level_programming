104f3c3025de0ecc5967dd7707d7c7c7281172da
Ayodelefasulu <fasuluayodele@gmail.com>
2023-11-28 18:57:12 -0800

    task 6 amended

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == j or j == i:
-            continue
-        print(f"{i}{j}, ", end="")
+for i in range(9):
+    for j in range(i + 1, 10):
+        print("{:02d}, ".format(i * 10 + j), end="" if j < 9 else "\n")
