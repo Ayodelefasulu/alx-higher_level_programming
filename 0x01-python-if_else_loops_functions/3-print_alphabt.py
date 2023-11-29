dd1cededb2fb107dd42e8cd95eec10d6606b4a0c
Ayodelefasulu <fasuluayodele@gmail.com>
2023-11-28 18:27:31 -0800

    task 3 amended

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 for char_code in range(ord('a'), ord('z') + 1):
     if chr(char_code) == 'q' or chr(char_code) == 'e':
         continue
-    print(chr(char_code), end="")
+    print("{}".format(chr(char_code)), end="")
