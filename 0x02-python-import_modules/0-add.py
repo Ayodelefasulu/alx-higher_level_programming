0c1dee82b2b7ce2c11862009e582e11ff2f19e9b
Ayodelefasulu <fasuluayodele@gmail.com>
2023-11-30 12:09:27 -0800

    PEP8 added

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 from add_0 import add
 
+
 def main():
+
     a = 1
     b = 2
     result = add(a, b)
     print("{:d} + {:d} = {:d}".format(a, b, result))
 
+
 if __name__ == "__main__":
+
     main()
