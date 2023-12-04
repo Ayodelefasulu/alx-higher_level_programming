87fcf9f9e9ba513f4010ca5382bad96c5605dd09
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-04 06:14:12 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,4 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-#    for a in matrix:
-#            print(ai)
-#        for bi in b:
-#            print(bi)
-#        for ci in c:
-#            print(ci)
-#         print(*a)
     print('{}'.format(a)) for a in matrix
