378f9d84af8f1b01788954411015140ca3cbe6b7
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-04 06:12:52 -0800

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 
 
 def no_c(my_string):
-    result_string = ''.join(char for char in my_string \
-        if char.lower() not in {'c', 'C'})
+    result_string =\
+        ''.join(char for char in my_string if char.lower() not in {'c', 'C'})
     return result_string
