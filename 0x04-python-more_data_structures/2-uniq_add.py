7ba2a4c0cc4e092d2ee0c980af9fb047407aaca0
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-06 08:42:47 -0800

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    s = list(set(my_list))
+    sum = 0
+    i = 0
+    while i < len(s):
+            sum = s[i] + sum
+            i = i + 1
+    print(s)
+    print(len(s))
+    return sum
+#    print(s)
