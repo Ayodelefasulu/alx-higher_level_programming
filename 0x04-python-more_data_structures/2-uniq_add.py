2b06eed273cdf9a05a253bd42977e5307877434b
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-06 17:57:26 -0800

    task 2 adjusted

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,13 +2,15 @@
 
 
 def uniq_add(my_list=[]):
-    s = list(set(my_list))
-    sum = 0
-    i = 0
-    while i < len(s):
-        sum = s[i] + sum
-        i = i + 1
-    print(s)
-    print(len(s))
-    return sum
-#    print(s)
+    # s = list(set(my_list))
+    # sum = 0
+    # i = 0
+    # while i < len(s):
+    #     # sum = s[i] + sum
+    #     # i = i + 1
+    # print(s)
+    # print(len(s))
+    # return sum
+    unique_set = set(my_list)
+    total_sum = sum(unique_set)
+    return total_sum
