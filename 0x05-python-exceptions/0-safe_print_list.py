29496e40b08940984d34f21c8d47e31f07fdc7a8
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-18 17:07:33 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,20 +2,6 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    for num in my_list:
-        try:
-            if my_list.index(num) == x:
-                break
-            else:
-                numb = print(int(num), end="")
-                a = a + 1
-        except IndexError:
-            print("This is not a number")
-    print("\n")
-    return a
-
-"""def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for i in range(x):
@@ -24,4 +10,5 @@ def safe_print_list(my_list=[], x=0):
     except IndexError:
         pass
     finally:
-"""
+        print()
+        return count
