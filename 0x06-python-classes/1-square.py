147c9494ad3c1e4f921c9022541b160e1d06c173
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-19 18:21:38 -0800

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,39 @@
 #!/usr/bin/python3
 
+"""Module: 1-square
+
+This module defines a class, Square, representing a square with a private size attribute.
+
+Classes:
+    Square: A class for creating square objects with a private size attribute.
+
+Attributes:
+    None.
+
+Methods:
+    __init__(self, size): Instantiates a Square with a given size.
+
+Notes:
+    - The size attribute is private, and its name is modified using name mangling
+      to include the class name, ensuring privacy and encapsulation.
+
+Example:
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
+"""
+
+
 class Square:
     """This is a square class."""
     def __init__(self, size):
