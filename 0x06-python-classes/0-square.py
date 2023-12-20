26b35d0d5ccd7e4cc53e09d132e319e2ced371ee
Ayodelefasulu <fasuluayodele@gmail.com>
2023-12-19 17:38:45 -0800

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
 class Square:
-    """A class representing a square."""
+    """A class representing a square.
+
+    This class does not contain any specific attributes or
+    methods at the moment,
+    but it can be used as a blueprint for creating square objects.
+
+    Example:
+    --------
+    # Create a Square object
+    my_square = Square()
+
+    Note:
+    -----
+    This class is a placeholder and may be extended with additional
+    attributes
+    and methods to represent specific features of a square in the future.
+    """
