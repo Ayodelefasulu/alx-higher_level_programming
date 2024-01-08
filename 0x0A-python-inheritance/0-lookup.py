c57112d89549bd8314569dfe1c933e7308721d63
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-08 07:18:43 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def lookup(obj):
+    """This function returns a list attrs and methods of objs"""
+
+    """" Parameter:
+           obj: object to return attrs and methods list.
+    """
+
     return dir(obj)
