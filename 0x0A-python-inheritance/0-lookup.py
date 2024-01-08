139598f417d58125959d38215e07381b93f670e6
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-08 07:32:33 -0800

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""This module defines a function called lookup.
+
+It takes obj as input and returns a list of all
+available attributes and methods.
+"""
+
 def lookup(obj):
     """This function returns a list attrs and methods of objs"""
 
