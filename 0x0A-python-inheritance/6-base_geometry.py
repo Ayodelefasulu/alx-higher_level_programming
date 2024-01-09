9a753a2d843889d170461188dbc1f80b1469ada6
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-08 17:44:32 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module defining the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Class definition for BaseGeometry."""
+
+    def area(self):
+        """Raises an Exception with the message:
+           'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
