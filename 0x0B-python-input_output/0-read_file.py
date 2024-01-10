2867e9d2f7986db73cd7f88ce06dcaf854ee695d
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-09 16:32:54 -0800

    Task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""This is an input/output module
+
+Functions:
+    read_file: That reads file line by line
+
+Argument(s):
+    filename: This is the name of the file
+
+Returns: None
+"""
+
+def read_file(filename=""):
+    """This function prints the lines of a file"""
+
+    with open(filename, "r") as f:
+        for l in f:
+            print(l, end="")
