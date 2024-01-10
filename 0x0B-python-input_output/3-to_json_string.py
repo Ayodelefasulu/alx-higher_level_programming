69d85774581a66bf4c667eedcfc43056572dd729
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-09 17:10:45 -0800

    Task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module: to_json_string
+
+Provides a function to convert an object to its JSON representation.
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+
+    Args:
+        my_obj: The object to be converted to JSON.
+
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
