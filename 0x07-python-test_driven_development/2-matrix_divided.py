b2b08cd1b7a27f82d506fc6ff6d714e8d5f11cd0
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-10 18:42:28 -0800

    task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,8 @@ def matrix_divided(matrix, div):
 
     Args:
         matrix (list of lists): The matrix to be divided.
-        div (int or float): The number by which each element of the matrix will be divided.
+        div (int or float):
+            The number by which each element of the matrix will be divided.
 
     Returns:
         list of lists: A new matrix with elements rounded to 2 decimal places.
@@ -22,8 +23,10 @@ def matrix_divided(matrix, div):
         [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
     """
     # Validate input types
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list)\
+    or not all(isinstance(row, list) for row in matrix):
+        raise\
+            TypeError("matrix must be matrix:list of lists of integers/floats")
 
     # Validate that each row has the same size
     row_size = len(matrix[0])
@@ -37,6 +40,7 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     # Perform matrix division and round to 2 decimal places
-    result_matrix = [[round(element / div, 2) for element in row] for row in matrix]
+    result_matrix =\
+        [[round(element / div, 2) for element in row] for row in matrix]
 
     return result_matrix
