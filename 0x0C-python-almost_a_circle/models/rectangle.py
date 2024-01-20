a57341ceb17794c34e79f4b45459e49476a5cb1b
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-19 17:51:04 -0800

    task 7

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,8 +112,10 @@ class Rectangle(Base):
     def display(self):
         """prints in stdout the character '#'."""
 
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print('#' * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         """Prints a string representation of object"""
