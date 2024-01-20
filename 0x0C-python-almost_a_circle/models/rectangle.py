f54c1bd6295014dc8115854a74af6beb2f2f4ccc
Ayodelefasulu <fasuluayodele@gmail.com>
2024-01-19 18:24:29 -0800

    task 11

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,3 +135,14 @@ class Rectangle(Base):
         if kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+   def to_dictionary(self):
+        """Return the dictionary representation of the Rectangle."""
+
+        return {
+            'id': self.id,
+            'width': self.__width,
+            'height': self.__height,
+            'x': self.__x,
+            'y': self.__y
+        }
