8c9f36391e263eee7b05675fc79ce11d3bbdb059
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-15 02:27:56 -0800

    task 4

--- 0x0E-SQL_more_queries/4-never_empty.sql
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -1,7 +1,5 @@
 -- create_id_not_null_table.sql
 
-USE `hbtn_0d_2`; -- Change this to your desired database name
-
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS `id_not_null` (
     `id` INT DEFAULT 1,
