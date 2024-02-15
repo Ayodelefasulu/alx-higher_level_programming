a7cb04d8e386fcfde039c931d7eca0099e897058
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-15 03:13:07 -0800

    task 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,7 @@
 -- list_all_cities.sql
 
 SELECT cities.id, cities.name, states.name AS state_name
-FROM cities, states
-WHERE cities.state_id = states.id
+FROM cities
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
+
