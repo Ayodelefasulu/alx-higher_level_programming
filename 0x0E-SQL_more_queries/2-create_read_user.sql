b372fc8705b84e9ece85eca8b7cd8173924dffe2
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-15 02:04:55 -0800

    task 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -6,12 +6,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Create user user_0d_2 if it doesn't exist
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant USAGE privilege to user_0d_2 on all databases
-GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
-
 -- Grant SELECT privilege to user_0d_2 on database hbtn_0d_2
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
--- Grant CREATE privilege to user_0d_2 on database hbtn_0d_2
-GRANT CREATE ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
