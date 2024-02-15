513a79080d1a11b75b7b6a9d38728f3a34a6f9fa
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-14 21:26:59 -0800

    task 0

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- list_shows_with_genre.sql
+
+USE hbtn_0d_tvshows;
+
+SELECT DISTINCT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
