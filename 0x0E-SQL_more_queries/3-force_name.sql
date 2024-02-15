6312512b1674ea2de4812c85244b2065c37e593e
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-15 02:13:20 -0800

    task 2

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,6 +1,7 @@
 -- create_force_name_table.sql
 
-USE `hbtn_0d_2`; -- Change this to your desired database name
+-- Use the database created
+USE `hbtn_0d_2`;
 
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS `force_name` (
