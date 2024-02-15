a4546dd7a6e2efa88521d5678505dd864d2b4d55
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-15 02:31:05 -0800

    task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,5 @@
 -- create_unique_id_table.sql
 
-USE `hbtn_0d_2`; -- Change this to your desired database name
-
 -- Create the table if it doesn't exist
 CREATE TABLE IF NOT EXISTS `unique_id` (
     `id` INT DEFAULT 1 UNIQUE,
