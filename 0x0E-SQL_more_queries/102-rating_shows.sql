0eac3671ac6c8d0e9ca6bb2147c07749cb16c210
Ayodelefasulu <fasuluayodele@gmail.com>
2024-02-17 00:41:19 -0800

    task 102

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -6,3 +6,8 @@ LEFT JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
 ORDER BY rating_sum DESC;
 
+SELECT tv_shows.title,
+       (SELECT SUM(rate) FROM tv_show_ratings WHERE show_id = tv_shows.id) AS rating_sum
+FROM tv_shows
+ORDER BY rating_sum DESC;
+
