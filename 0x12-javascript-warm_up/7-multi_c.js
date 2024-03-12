33702e241746ac87df2e7f80bab172979b75e581
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-11 20:39:06 -0700

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const x = parseInt(process.argv[2]);
+
+if (!isNaN(x) && x > 0) {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
+} else if (x < 0) {
+  return
+}
+  else {
+  console.log('Missing number of occurrences');
+}
