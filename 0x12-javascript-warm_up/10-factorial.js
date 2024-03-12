8db92df86288db6349d5c6c4959e7ec4d12dc652
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-11 21:09:24 -0700

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else if (n < 0) {
+    return Infinity;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
