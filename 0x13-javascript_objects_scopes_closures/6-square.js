4fca913f20ac3fd0dbc5da00882d5a017dca338a
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-13 20:16:34 -0700

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const parentSquare = require('./5-square');
+
+class Square extends parentSquare {
+  constructor (size) {
+    super (size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
