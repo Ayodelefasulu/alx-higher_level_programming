c0d7eb8e01e062b71fc4e1172e9b5e89df4bf87d
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-16 19:40:01 -0700

    task 1

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""
+Script that takes an argument and displays all values...
+   in the states table of hbtn_0e_0_usa where name matches the argument.
+
+Usage:
+    ./2-my_filter_states.py <username> <password> <database_name> <state_name>
+
+Example:
+    ./2-my_filter_states.py root root hbtn_0e_0_usa 'Arizona'
+"""
+
+import MySQLdb
+import sys
+
+
+def filter_states(username, password, database_name, state_name):
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database_name
+        )
+
+        # Create a cursor object
+        cursor = db.cursor()
+
+        # Execute SQL query to filter states based on provided state name
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+        # Close the cursor and connection
+        cursor.close()
+        db.close()
+
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: ./2-my_filter_states.py\
+            <username> <password> <database_name> <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    filter_states(username, password, database_name, state_name)
