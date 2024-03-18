c8e75d637eb7abc8f622b889ac4c41e8ade229bb
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-17 20:51:58 -0700

    task 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database_n>".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
     # Database connection details
