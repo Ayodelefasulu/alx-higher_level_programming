355fe11e6a2d9743bb630e1f6553a92ca77a40a0
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-17 19:33:01 -0700

    task7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -9,14 +9,16 @@ from model_state import Base, State
 if __name__ == "__main__":
     # Check if correct number of arguments are provided
     if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>"
+            .format(sys.argv[0]))
         sys.exit(1)
 
     # Database connection details
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, dbname))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(username, password, dbname))
 
     # Create session
     Session = sessionmaker(bind=engine)
@@ -29,4 +31,3 @@ if __name__ == "__main__":
 
     # Close session
     session.close()
-
