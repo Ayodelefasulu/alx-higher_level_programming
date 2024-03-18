158db5f92a57caf792e2f6f96bc36acbb4294fee
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-17 20:50:04 -0700

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Add an object to the State with the specified name to the database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database_n>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Database connection details
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database_name))
+
+    # Create session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Insert object to database table
+    state_object = State(name="Louisiana")
+    session.add(state_object)
+    session.commit()  # Commit changes
+
+    if state_object:
+        print(state_object.id)
+
+    # Close connection
+    session.close()
