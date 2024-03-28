02948a50c8c0e85ffe958e5d66285126cea6b8fd
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-28 04:11:54 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display body for successful (200) GET request
-curl -s "$1" | grep -Ev '^HTTP.*$' | grep -Ev '^$'
+# Display body for successful (200) GET request (alternative)
+if [[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == 200 ]]; then curl -s "$1"; fi
