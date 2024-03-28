88c57178aa2453dd75330918a966cf54965c51d9
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-28 04:37:05 -0700

    task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script retrieves and displays all HTTP methods accepted by the server for a given URL
-curl -sI "$1" | grep -i Allow | awk '{print $2}'
+curl -s -X OPTIONS -i "$1" | grep -i Allow | cut -d':' -f2- | tr -d '\r\n'
