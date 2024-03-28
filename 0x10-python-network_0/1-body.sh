a2b63c3a55e39b77ca488fea3749fd4ec4fa79d1
Ayodelefasulu <fasuluayodele@gmail.com>
2024-03-28 04:05:43 -0700

    task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script sends a GET request to a URL and displays the body of the response for a 200 status code
-curl -s -o response.txt -w "%{http_code}" "$1" | grep -q "200" && cat response.txt
+# Display body of 200 response from URL
+curl -s -w "%{http_code}" "$1" -o /dev/null | grep 200 >/dev/null && curl -s "$1"
