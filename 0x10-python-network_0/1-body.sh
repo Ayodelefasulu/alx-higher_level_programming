1401ad93287953241a17300bad551d8ebb083488
Ayodelefasulu <fasuluayodele@gmail.com>
2024-04-02 21:19:09 -0700

    task 3

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display body for successful (200) GET request (alternative)
-if [[ $(curl -s -o /dev/null -w "%{http_code}" "$1") == 200 ]]; then curl -s "$1"; fi
+curl -Lsf "$1"
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script retrieves and displays all HTTP methods accepted by the server for a given URL
-curl -s -X OPTIONS -i "$1" | grep -i Allow | cut -d':' -f2- | tr -d '\r\n'
+curl -sI "$1" | grep -i "Allow" | awk -F ": " '{ print $2 }'
