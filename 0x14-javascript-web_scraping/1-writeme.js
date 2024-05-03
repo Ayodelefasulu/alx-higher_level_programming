ace660c934d4d10104eb6af58f9caaa595e1cf50
Ayodelefasulu <fasuluayodele@gmail.com>
2024-05-03 01:18:21 -0700

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,24 +1,19 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-// Function to write a string to a file
-function writeToFile(filePath, content) {
-  fs.writeFile(filePath, content, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('The file has been successfully written.');
-  });
-}
-
-// Main execution
-if (process.argv.length !== 4) {
-  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-writeToFile(filePath, content);
+if (!filePath || !content) {
+  console.error('Usage: node 1-writeme.js <file_path> "<content_string>"');
+  process.exit(1);
+}
+
+fs.writeFile(filePath, content, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Successfully wrote content to ${filePath}`);
+  }
+});
+
